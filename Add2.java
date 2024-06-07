5d68ac9fc2ca33ef5df6f783ae1c71e996e0cd6f
Bhatt Vishwa <105030732+BhattVishwa28@users.noreply.github.com>
2024-06-07 22:29:30 +0530

    Add files via upload

---
+++ Add2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Add2{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a");
+		int a = sc.nextInt();
+		System.out.println("Enter b");
+		int b = sc.nextInt();
+		int x = sc.nextInt();
+		x = a+b;
+		System.out.println("x");
+	}
+}
